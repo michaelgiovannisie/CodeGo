0bcbec2b57d4fd00bacd3484b2147ac7c91c7371
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-04-09 17:20:07 -0400

    done with jshell and JavaSmall

---
+++ java/helloWorld.java
@@ -0,0 +1,9 @@
+System.out.println("Hello, World!");
+int x = 5;
+x
+x+5
+x
+x=x+10
+x
+String s = "Godlike";
+System.out.println(s);
---
+++ java/sumNumbers.java
@@ -0,0 +1,132 @@
+System.out.println("Hello, World!");
+x
+x+5
+x
+x=x+10
+x
+System.out.println(s);
+while (i<=10) {
+    System.out.println(i);;
+    i++;
+}
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5)
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+sumNumbersTo10();
+System.out.println("Hello, World!");
+int x = 5;
+x
+x+5
+x
+x=x+10
+x
+String s = "Godlike";
+System.out.println(s);
+while (i<=10) {
+    System.out.println(i);;
+    i++;
+}
+void printNumbers() {
+    int i = 1;
+    while (i <= 10) {
+        System.out.println(i);
+        i++;
+    }
+}
+void printNumbers(int n) {
+    int i=1;
+    while (i <= n) {
+        System.out.println(i);
+        i++;
+    }
+}
+printNumbers(5);
+printNumbers(10);
+printNumbers(20);
+sumNumbers(5);
+sumNumbers(11);
+sumNumbers(23);
+sumNumbers(5683);
+sumNumbers(106839);
+sumNumbers(5)
+sumNumbers(5);
+sumNumbers(10);
+sumNumbers(20);
+sumNumbers(106839);
+int sumNumbers(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+int sumNumbersTo10() {
+    return sumNumbers(10);
+}
+int sumNumbersTo13() {
+    return sumNumbers(13);
+}
+sumNumbersTo13()
+addTwoNumber(5,10);
+int addTwoNumber(int x, int y) {
+    int sum;
+    sum = x + y;
+    return sum;
+}
+addTwoNumber(5,10);
+addTwoNumber(27348, 63476);
+divideTwoNumbers(7342, 4)
+divideTwoNumbers(7438, 5)
+float divideTwoNumbers(int x, int y) {
+    float div;
+    div = x / y;
+    return div;
+}
+divideTwoNumbers(4325,6)
+multiplyTwoNumbers(25,25)
+int multiplyTwoNumbers(int x, int y) {
+    int mul;
+    mul = x * y;
+    return mul;
+}
+multiplyTwoNumbers(25,25)
+int subtractTwoNumbers(int x, int y) {
+    int z;
+    z = x - y;
+    return z;
+}
+subtractTwoNumbers(462,73);
+boolean isEven (int x) {
+    return x % 2 == 0;
+}
+isEven(523);
+boolean isOdd (int x) {
+    return x % 2 == 1;
+}
+isOdd(1321);
+int i = 1;
+while (i <= 10) {
+    System.out.println(i + " % 2 = " + i % 2);
+    i++;
+}
+void printOddEven() {
+    int i = 1;
+    while (i <= 10) {
+        if (isEven(i)) {
+            System.out.println(i + " is even" );
+        } else {
+            System.out.println(i + "is odd");
+        }
+        i++;
+    }
+}
